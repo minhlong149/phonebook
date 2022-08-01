f24d741afcca5e293c2bffb9bf467662e0508e14
minhlong149 <minhlong9514@gmail.com>
2022-08-01 15:36:25 +0700

    Add new phonebook entries

--- index.js
+++ index.js
@@ -57,6 +57,29 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+// Add new phonebook entries
+app.use(express.json());
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const maxId = Math.floor(Math.random() * 1000);
+  const newEntry = {
+    id: maxId,
+    name: body.name,
+    number: body.number,
+  };
+
+  phonebook = phonebook.concat(newEntry);
+  response.json(newEntry);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
