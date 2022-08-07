1805ed3db66ef19ab5dcbbe61c43c4998d081989
minhlong149 <minhlong9514@gmail.com>
2022-08-07 13:54:57 +0700

    Add new production build

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("build"));
+
 // Logging messages using morgan middleware
 const morgan = require("morgan");
 app.use(
